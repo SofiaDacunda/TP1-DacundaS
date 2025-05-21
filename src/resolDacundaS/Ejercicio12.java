e8705c28f45333daf2d7f8c2046b274cb766d8f6
Sofia <dacundasofia4@gmail.com>
2025-05-21 13:36:57 -0300

    commit

---
+++ src/resolDacundaS/Ejercicio12.java
@@ -0,0 +1,30 @@
+package resolDacundaS;
+
+import java.util.Scanner;
+
+public class Ejercicio12 {
+    public static void ejecutar() {
+        Scanner sc = new Scanner(System.in);
+        int[] numeros = new int[5];
+
+        System.out.println("Ingrese 5 números enteros:");
+        for (int i = 0; i < 5; i++) {
+            numeros[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < numeros.length - 1; i++) {
+            for (int j = 0; j < numeros.length - 1 - i; j++) {
+                if (numeros[j] < numeros[j + 1]) {
+                    int temp = numeros[j];
+                    numeros[j] = numeros[j + 1];
+                    numeros[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println("Números ordenados de mayor a menor:");
+        for (int num : numeros) {
+            System.out.print(num + " ");
+        }
+    }
+}
---
+++ src/resolDacundaS/Ejercicio14.java
@@ -0,0 +1,67 @@
+package resolDacundaS;
+
+public class Ejercicio14 {
+
+    public static void ejecutar() {
+        int[][] original = {
+                {1, 2, 0, 3, 0, 7},
+                {0, 0, 0, 0},
+                {0, 6, 9, 8, 0, 9, 6},
+                {0, 0, 0, 0, 7, 9, 0, 0},
+                {0, 0, 0, 0, 0},
+                {8, 7, 0, 8, 9, 0, 8}
+        };
+
+        System.out.println("Arreglo original:");
+        mostrar(original);
+
+        int filasValidas = 0;
+        for (int i = 0; i < original.length; i++) {
+            boolean filaSoloCeros = true;
+            for (int j = 0; j < original[i].length; j++) {
+                if (original[i][j] != 0) {
+                    filaSoloCeros = false;
+                    break;
+                }
+            }
+            if (!filaSoloCeros) {
+                filasValidas++;
+            }
+        }
+
+        int[][] resultado = new int[filasValidas][];
+        int filaResultado = 0;
+
+        for (int i = 0; i < original.length; i++) {
+            int cantidadNoCeros = 0;
+            for (int j = 0; j < original[i].length; j++) {
+                if (original[i][j] != 0) {
+                    cantidadNoCeros++;
+                }
+            }
+
+            if (cantidadNoCeros > 0) {
+                resultado[filaResultado] = new int[cantidadNoCeros];
+                int idx = 0;
+                for (int j = 0; j < original[i].length; j++) {
+                    if (original[i][j] != 0) {
+                        resultado[filaResultado][idx++] = original[i][j];
+                    }
+                }
+                filaResultado++;
+            }
+        }
+
+        System.out.println("Arreglo sin ceros y sin filas solo con ceros:");
+        mostrar(resultado);
+    }
+
+    public static void mostrar(int[][] arreglo) {
+        for (int[] fila : arreglo) {
+            for (int num : fila) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}
